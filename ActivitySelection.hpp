11ee13c9b4eb2e080a3e5e970467f3ab5d76aa86
Shu Liu <liushu_public@yeah.net>
2023-10-24 18:25:49 +0800

    init

---
+++ ActivitySelection.hpp
@@ -0,0 +1,15 @@
+// Activity Selection Problem:
+//  Input:
+//      - A set of activities: S = {a1, a2,..., an}
+//      - The start time si and finish time fi of activity ai
+//  Output:
+//      - Subset of activities S', where for each ai and aj, si >= fj or fi <= fj
+//      - Maximize |S'|
+
+
+#ifndef ALGORITHMDESIGN_ACTIVITYSELECTION_HPP
+#define ALGORITHMDESIGN_ACTIVITYSELECTION_HPP
+
+
+
+#endif //ALGORITHMDESIGN_ACTIVITYSELECTION_HPP
